ea9402f5b1374db45e22f54916319114498d49e3
Jimmy_Duarte_Ramirez <jimmy.duarte86@gmail.com>
2026-07-19 23:06:10 -0400

    septimo commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -39,7 +39,7 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   const { username, password } = req.body;
   try {
-    const userFound = await prisma.user.findUnique({ where: { username } });
+    const userFound = await prisma.user.findFirst({ where: { username } });
     if (!userFound) return res.status(400).json({ message: 'Usuario no encontrado' });
 
     const isMatch = await bcrypt.compare(password, userFound.password);
